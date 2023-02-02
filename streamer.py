cb26d8f3f4ae04706159fab2c33683529d106c2a
eraser <minecraft177@yandex.ru>
2023-02-02 14:29:28 +0300

    streamer works

--- streamer.py
+++ streamer.py
@@ -1,55 +1,50 @@
 # import required libraries
-from vidgear.gears import VideoGear
+from vidgear.gears import CamGear
 from vidgear.gears import NetGear
 import sys
 
-ssh_con = "root@78.140.241.126:22"
+def run():
+    ssh_con = "root@78.140.241.126"
 
-if len(sys.argv) > 1:
-    ssh_con = sys.argv[1]
-else:
-    print(ssh_con + " used")
+    if len(sys.argv) > 1:
+        ssh_con = sys.argv[1]
+    else:
+        print(ssh_con + " used")
 
-# activate SSH tunneling with SSH URL, and
-# [BEWARE!!!] Change SSH URL and SSH password with yours for this example !!!
-options = {
-    "ssh_tunnel_mode": ssh_con, # defaults to port 22
-    "ssh_tunnel_keyfile":"~/.ssh/id_rsa",
-}
+    options = {
+        "ssh_tunnel_mode": ssh_con, # defaults to port 22
+        "ssh_tunnel_keyfile":'C:/Users/79922/.ssh/id_rsa',
+    }
 
-# Open live video stream on webcam at first index(i.e. 0) device
-stream = VideoGear(source=0).start()
+    # Open live video stream on webcam at first index(i.e. 0) device
+    stream = CamGear(source=0).start()
 
-# Define NetGear server at given IP address and define parameters
-server = NetGear(
-    address="127.0.0.1", # don't change this
-    port="5454",
-    pattern=1, 
-    logging=True, 
-    **options
-)
+    # Define NetGear server at given IP address and define parameters
+    server = NetGear(
+        address="127.0.0.1", # don't change this
+        port="5454",
+        pattern=2,
+        logging=True,
+        **options
+    )
 
-# loop over until KeyBoard Interrupted
-while True:
+    # loop over until KeyBoard Interrupted
+    while True:
+        try:
+            # read frames from stream
+            frame = stream.read()
 
-    try:
-        # read frames from stream
-        frame = stream.read()
+            # check for frame if Nonetype
+            if frame is None:
+                break
+            # send frame to server
+            server.send(frame)
 
-        # check for frame if Nonetype
-        if frame is None:
+        except KeyboardInterrupt:
+            server.close()
+            stream.stop()
             break
 
-        # {do something with the frame here}
 
-        # send frame to server
-        server.send(frame)
-
-    except KeyboardInterrupt:
-        break
-
-# safely close video stream
-stream.stop()
-
-# safely close server
-server.close()
+if __name__ == '__main__':
+    run()
